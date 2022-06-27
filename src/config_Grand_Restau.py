a66d8ac2bee5430bf4532aa163b61b2dcb4dd79a
rdanf1 <reylet.daniel@gmail.com>
2022-06-27 16:04:01 +0200

    ALL_ITEMS : DONE !

---
+++ src/config_Grand_Restau.py
@@ -0,0 +1,40 @@
+ALL_ITEMS = {
+  'Entrées': (
+    ('Salade verte', 2),
+    ('Salade tomates oignons', 4),
+    ('Salade mozza basilic', 6),
+    ('Salade crudités', 6),
+    ('Salade composée', 8),
+    ('Avocat pesto', 8)
+  ),
+  'Légumes/Pâtes': (
+    ('Pâtes nature', 2.0),
+    ('Pâtes tomates oignons basilic', 4.0),
+    ('Pâtes carbonara', 6.0),
+    ('Buffet de legumes', 6.0),
+  ),
+  'Poissons': (
+    ('Panées de Cabillau', 4.0),
+    ('Panées de Colin', 6.0),
+    ('Steak de Thon', 8.0),
+    ('Truite saumonée', 8.0)
+  ),
+  'Desserts': (
+    ('Yaourt/Compote', 2.0),
+    ('Tartelette', 3.0),
+    ('Cake aux fruits', 3.0),
+    ('Café gourmand', 6.0)
+  ),
+  'Boissons': (
+    ("'Carafe d'O", 0.0),
+    ('Jus de fruit', 2.0),
+    ('Eau minerale', 2.0),
+    ('Café', 1.5)
+  )
+}
+
+MENUS = (
+    {"ceci", "cela"},
+    {'cela', 'ceci'}
+)
+
